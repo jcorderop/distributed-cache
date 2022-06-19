7e3b1f1b92e3b9082cb25056e8f2b0e3ceaa20ea
jcorderop <iorch.cordero@gmail.com>
2022-06-19 17:19:30 +0200

    Initial commit

---
+++ src/test/java/com/caching/distributed/DistributedCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caching.distributed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
